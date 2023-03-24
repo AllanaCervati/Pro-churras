0deda952971e6377ac11296989518544491c4021
Allana Cervati <allana.cervati@etec.sp.gov.br>
2023-03-24 19:57:07 -0300

    Aula Ronan

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'bru-churras',
+  appName: 'pru-churras',
   webDir: 'www',
   bundledWebRuntime: false
 };
